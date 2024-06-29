adee5b44f11bf0ca4cb54f2385cd3343e6764eb4
Raman Singh Chauhan <ramansinghchauhan787@gmail.com>
2024-06-29 09:36:54 +0545

    Not done yet

--- passwordGen/src/App.jsx
+++ passwordGen/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useCallback } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setLength] = useState(6);
+  
+  const [charAllowed, setcharAllowed] = useState(false);
+  const [password,setPassword]=useState("")
+  const passwordGenerator=useCallback(fn,[])
+  
 
   return (
    <>
-   <h1 className='bg-amber-400 text-red-500'>Raman</h1>
-
-   <h2>Hey Man</h2>
+   
+   <h1 className='bg-amber-400 text-gray-50 text-xl'>Raman</h1>
+   <h2 className='text-gray-50'>Hey Man</h2>
+   
    </>
   )
 }
